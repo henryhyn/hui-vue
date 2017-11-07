760212b8c9ed75f33ed253338a195de9ecbef106
何永能 <yongneng.he@dianping.com>
2017-11-07 15:32:48 +0800

    toggleHeading

--- src/components/functions.js
+++ src/components/functions.js
@@ -2,8 +2,9 @@ const Command = 'Cmd';
 
 export default {
   toolbar: [
-    {name: 'bold', key: [Command, 'B'], action: 'toggleBold'},
-    {name: 'italic', key: [Command, 'I'], action: 'toggleItalic'}
+    {name: 'Bold', key: [Command, 'B'], action: 'toggleBold'},
+    {name: 'Italic', key: [Command, 'I'], action: 'toggleItalic'},
+    {name: 'Heading', key: [Command, 'H'], action: 'toggleHeading'}
   ].map(i => {
     const mac = i.key.join('-');
     const win = mac.replace('Cmd', 'Ctrl');
@@ -12,7 +13,12 @@ export default {
 
   methods: {
     toggleBold () {
-      if (!this.selection.isEmpty()) {
+      if (this.selection.isEmpty()) {
+        const {row, column} = this.selection.getCursor();
+        this.editor.insert('****');
+        this.editor.moveCursorTo(row, column + 2);
+        this.editor.focus();
+      } else {
         const range = this.selection.getRange();
         const text = this.session.getTextRange(range);
         this.session.replace(range, `**${text}**`);
@@ -21,12 +27,34 @@ export default {
     },
 
     toggleItalic () {
-      if (!this.selection.isEmpty()) {
+      if (this.selection.isEmpty()) {
+        const {row, column} = this.selection.getCursor();
+        this.editor.insert('**');
+        this.editor.moveCursorTo(row, column + 1);
+        this.editor.focus();
+      } else {
         const range = this.selection.getRange();
         const text = this.session.getTextRange(range);
         this.session.replace(range, `*${text}*`);
         this.selection.clearSelection();
       }
+    },
+
+    toggleHeading () {
+      const {row} = this.selection.getCursor();
+      const lineText = this.session.getLine(row);
+      const length = lineText.length;
+      const level = lineText.search(/[^#]/);
+      if (level >= 6) {
+        return;
+      }
+      const heading = level <= 0 ? '# ' : '#';
+      const range = {
+        start: {row, column: 0},
+        end: {row, column: length}
+      };
+      this.session.replace(range, heading + lineText);
+      this.editor.focus();
     }
   }
 };
