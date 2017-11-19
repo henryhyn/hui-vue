6e32102d1c5411a3a407094224bf8bc198b0cc3b
何永能 <yongneng.he@dianping.com>
2017-11-19 15:02:50 +0800

    Regexp

--- src/utils/Hex.js
+++ src/utils/Hex.js
@@ -76,6 +76,16 @@ Hex.delete = (url, cb) => {
 
 Hex.px = n => /^\d+$/.test(n) ? `${n}px` : n;
 
+// 参考: https://segmentfault.com/a/1190000011966867
+// 判断是否为邮箱地址
+Hex.isEmail = str => /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/.test(str);
+// 判断是否为身份证号
+Hex.isIdCard = str => /^(^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$)|(^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])((\d{4})|\d{3}[Xx])$)$/.test(str);
+// 判断是否为手机号
+Hex.isPhoneNum = str => /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/.test(str);
+// 判断是否为URL地址
+Hex.isUrl = str => /[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i.test(str);
+
 Hex.validAny = any => any !== null && any !== undefined;
 Hex.validString = str => Hex.validAny(str) && str && str.toLowerCase() !== 'null' && str.toLowerCase() !== 'undefined' && str.trim().length > 0;
 Hex.validNumber = num => Hex.validAny(num);
