ac36dd1dd99b133415808fb9d203fd4c79fa1ced
Henry He <henryhyn@163.com>
2023-08-18 16:32:47 +0800

    feat: v2.0.7

--- src/utils/refiner.js
+++ src/utils/refiner.js
@@ -1,6 +1,19 @@
 const CRLF_LF = /[ ]*\r?\n/g;
-const CJK_ANS = /(\p{Unified_Ideograph})(\w)/gui;
-const ANS_CJK = /(\w%?)(\p{Unified_Ideograph})/gui;
+const CJK_ANS = /(\p{Unified_Ideograph})(\w)/gu;
+const ANS_CJK = /(\w%?)(\p{Unified_Ideograph})/gu;
+const BRACKET = /（(.+?)）/gu;
+const CJK_BRACKET = /(\p{Unified_Ideograph}\*{0,2})([$(])/gu;
+const BRACKET_CJK = /([$)])(\p{Unified_Ideograph})/gu;
+
+const PUNCT = /(\p{Unified_Ideograph})([,.?:])[ ]*/gu;
+const PUNCT_MAP = {
+  ',': '，',
+  '.': '。',
+  '?': '？',
+  '!': '！',
+  ';': '；',
+  ':': '：'
+};
 
 class Refiner {
   constructor() {
@@ -8,7 +21,7 @@ class Refiner {
   }
 
   refine(text) {
-    return this.spacing(text);
+    return this.convert(this.spacing(text));
   }
 
   spacing(text) {
@@ -16,7 +29,14 @@ class Refiner {
     newText = newText.replace(CRLF_LF, '\n');
     newText = newText.replace(CJK_ANS, '$1 $2');
     newText = newText.replace(ANS_CJK, '$1 $2');
+    newText = newText.replace(BRACKET, '($1)');
+    newText = newText.replace(CJK_BRACKET, '$1 $2');
+    newText = newText.replace(BRACKET_CJK, '$1 $2');
     return newText + '\n';
+  }
+
+  convert(text) {
+    return text.replace(PUNCT, (match, p1, p2) => `${p1}${PUNCT_MAP[p2]}`);
   }
 }
 
